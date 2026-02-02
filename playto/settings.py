33901f92b186b56d758485a87adcfe088d97eaad
tharunkumar5757123 <tharun5757123@gmail.com>
2026-02-02 19:58:05 +0530

    updated code

--- playto/settings.py
+++ playto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ajhl56)(*8^q4#^$-=@%1jah759q0zd62ji7lxntofay6o-=a@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
